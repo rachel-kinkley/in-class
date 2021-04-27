0ee1f29421b922feab34c7b197d9a4f70f91ed0a
rachel-kinkley <rgkinkley@gmail.com>
2021-04-27 13:30:28 -0700

    day 9 follow along

--- day-09/day-09/js/demo.js
+++ day-09/day-09/js/demo.js
@@ -1,25 +1,42 @@
 'use strict';
 
+let body = document.querySelector('body');
+
+let paragraph = document.createElement('p');
+paragraph.className = 'highlight';
+paragraph.classList.add('important');
+paragraph.innerText = "hello JS world";
+paragraph.style.fontSize = '2em';
+
+body.appendChild(paragraph);
+
 // use querySelector and css selectors to get the none.css href
 // log it
-
-
-
+let css = document.quesrySelector('[href*="css/none.css"]');
+console.log(css);
 
 //select h1 and modify it's font-size
-
-
+let h1 = document.querySelector('h1');
+h1.style.fontSize = "5em";
 
 
 //add event listener to the button to track x and y of clicks
 //log them
 //log the target
+let button = document.querySelector('button');
+button.addEventListener('click', function() {
+    let message = "You clicked " + offsetX + " and " + offsetY;
 
+    let clickedElem = event.target;
+
+    console.log(message);
+    console.log(clicked);
+})
 
 
 
 //key press event listener
-
+document.addEventListener('keydown', )
 
 
 
